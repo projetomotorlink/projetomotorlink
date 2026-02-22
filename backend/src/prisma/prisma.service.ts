77de3a47f60440f1a93b2da745a0ec2f5be6ae0d
Fernando <fernando.goncalves@aluno.faculdadeimpacta.com.br>
2026-02-22 18:38:09 -0300

    hotfix: Update PrismaService to use new Prisma adapter and adjust tsconfig paths

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,20 +1,18 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@/generated/prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
+/* eslint-disable prettier/prettier */
+import { PrismaClient } from "@/generated/prisma/client/client";
+import { Injectable, OnModuleInit } from "@nestjs/common";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   constructor() {
     const adapter = new PrismaPg({ url: process.env.DATABASE_URL });
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     super({ adapter });
   }
   async onModuleInit() {
     await this.$connect();
   }
-
-  async onModuleDestroy() {
+  async onApplicationShutdown() {
     await this.$disconnect();
   }
 }
